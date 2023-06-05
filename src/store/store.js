d910ced58f1c6942afb8ca8960fcbec632055cba
tahirnoorkhaliq <tahirnoorkhaliq@gmail.com>
2023-06-05 15:32:17 +0530

    redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,19 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./root-reducer";
+import logger from "redux-logger";
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+/*import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
-
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
+
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -31,4 +37,23 @@ export const store = createStore(
   composedEnhancers
 );
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store); */
+
+export const store = configureStore({
+  reducer: rootReducer,
+  //1 way
+  //middleware:middleWares
+  //2nd way
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: false,
+  //   }),
+  //3rd way
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: false,
+  //   }).concat(middleWares),
+    //4th way
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleWares),
+});
